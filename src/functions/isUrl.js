bbef979f303f246288014df9e6d1c11884e72a20
Manuel Pascual <manupascual90@gmail.com>
2020-06-13 12:54:11 -0300

    Fix bug

--- src/functions/isUrl.js
+++ src/functions/isUrl.js
@@ -1,7 +1,8 @@
 const { regularExpressions } = require("../config");
 
-const isUrl = val => {
-  return regularExpressions.usernameRegex.test(val);
+const isUrl = (val, type) => {
+  if (!regularExpressions.urlRegex[type]) return false;
+  return regularExpressions.urlRegex[type].test(val);
 };
 
 module.exports = isUrl;
