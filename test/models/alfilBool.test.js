08d82c9b619576ec4b2d04dd461f10618d0493b5
Manuel Pascual <manupascual90@gmail.com>
2020-06-13 13:20:17 -0300

    Remove test describe

--- test/models/alfilBool.test.js
+++ test/models/alfilBool.test.js
@@ -1,27 +1,25 @@
 const AlfilBool = require("../../src/models/AlfilBool");
 
-describe("AlfilBool Tests", () => {
-  test("Testing isCorrectType against true returns true.", () => {
-    const instance = new AlfilBool();
-    const valueToTest = true;
-    expect(instance.isCorrectType(valueToTest)).toBeTruthy();
-  });
+test("Testing isCorrectType against true returns true.", () => {
+  const instance = new AlfilBool();
+  const valueToTest = true;
+  expect(instance.isCorrectType(valueToTest)).toBeTruthy();
+});
 
-  test("Testing isCorrectType against false returns true.", () => {
-    const instance = new AlfilBool();
-    const valueToTest = false;
-    expect(instance.isCorrectType(valueToTest)).toBeTruthy();
-  });
+test("Testing isCorrectType against false returns true.", () => {
+  const instance = new AlfilBool();
+  const valueToTest = false;
+  expect(instance.isCorrectType(valueToTest)).toBeTruthy();
+});
 
-  test("Testing true() against true returns true.", () => {
-    const instance = new AlfilBool().true();
-    const valueToTest = true;
-    expect(instance.runValidations(valueToTest)).toBeTruthy();
-  });
+test("Testing true() against true returns true.", () => {
+  const instance = new AlfilBool().true();
+  const valueToTest = true;
+  expect(instance.runValidations(valueToTest)).toBeTruthy();
+});
 
-  test("Testing false() against false returns true.", () => {
-    const instance = new AlfilBool().false();
-    const valueToTest = false;
-    expect(instance.runValidations(valueToTest)).toBeTruthy();
-  });
+test("Testing false() against false returns true.", () => {
+  const instance = new AlfilBool().false();
+  const valueToTest = false;
+  expect(instance.runValidations(valueToTest)).toBeTruthy();
 });
